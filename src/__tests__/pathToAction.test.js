0b6b06dd1bda114113f2d3d1f407e631dc127660
Martin <martin@sachmata.com>
2020-07-15 23:03:53 +0300

    Convert action and path conversion to use location object

--- src/__tests__/pathToAction.test.js
+++ src/__tests__/pathToAction.test.js
@@ -9,51 +9,61 @@ const routesMap = buildRoutesMap(
 
 describe('pathToAction', () => {
 	test('defined route, no params', () => {
-		expect(pathToAction(routesMap, '/portal/projects')).toEqual(navigate('PROJECTS'));
+		expect(
+			pathToAction(routesMap, {
+				pathname: '/portal/projects',
+			})
+		).toEqual(navigate('PROJECTS'));
 	});
 
 	test('defined route, with params', () => {
-		expect(pathToAction(routesMap, '/portal/projects/Project123')).toEqual(
-			navigate('PROJECT', { projectName: 'Project123' })
-		);
+		expect(
+			pathToAction(routesMap, {
+				pathname: '/portal/projects/Project123',
+			})
+		).toEqual(navigate('PROJECT', { projectName: 'Project123' }));
 	});
 
 	test('defined route, with zero or more params', () => {
-		expect(pathToAction(routesMap, '/portal/model')).toEqual(navigate('MODEL', {}));
+		expect(
+			pathToAction(routesMap, {
+				pathname: '/portal/model',
+			})
+		).toEqual(navigate('MODEL', {}));
 
-		expect(pathToAction(routesMap, '/portal/model/tag1/tag2/tag3')).toEqual(
-			navigate('MODEL', { tags: ['tag1', 'tag2', 'tag3'] })
-		);
+		expect(
+			pathToAction(routesMap, {
+				pathname: '/portal/model/tag1/tag2/tag3',
+			})
+		).toEqual(navigate('MODEL', { tags: ['tag1', 'tag2', 'tag3'] }));
 	});
 
 	test('defined route, with params, with search', () => {
-		expect(pathToAction(routesMap, '/portal/projects/Project123', '?returnTo=home')).toEqual(
+		expect(
+			pathToAction(routesMap, {
+				pathname: '/portal/projects/Project123',
+				search: '?returnTo=home',
+			})
+		).toEqual(
 			navigate('PROJECT', { projectName: 'Project123' }, { query: { returnTo: 'home' } })
 		);
 	});
 
 	test('defined route, with params, without search', () => {
-		expect(pathToAction(routesMap, '/portal/projects/Project123', '')).toEqual(
-			navigate('PROJECT', { projectName: 'Project123' })
-		);
-	});
-
-	test('defined route, with params, with state', () => {
 		expect(
-			pathToAction(routesMap, '/portal/projects/Project123', '?returnTo=home', {
-				custom: 123,
+			pathToAction(routesMap, {
+				pathname: '/portal/projects/Project123',
+				search: '',
 			})
-		).toEqual(
-			navigate(
-				'PROJECT',
-				{ projectName: 'Project123', custom: 123 },
-				{ query: { returnTo: 'home' } }
-			)
-		);
+		).toEqual(navigate('PROJECT', { projectName: 'Project123' }));
 	});
 
 	test('undefined route', () => {
-		expect(pathToAction(routesMap, '/portal/undefined')).toEqual(navigate('NOT_FOUND'));
+		expect(
+			pathToAction(routesMap, {
+				pathname: '/portal/undefined',
+			})
+		).toEqual(navigate('NOT_FOUND'));
 	});
 
 	test('NULL path', () => {
--- src/core.js
+++ src/core.js
@@ -3,7 +3,7 @@ import { compile as p2rCompile, match as p2rMatch } from 'path-to-regexp';
 export const NAVIGATE = 'router/NAVIGATE';
 
 export function navigate(id, params = {}, opts = {}) {
-	return { type: NAVIGATE, id, params, ...opts };
+	return { type: NAVIGATE, id, params, query: null, ...opts };
 }
 
 export function route(id, path, navigateSaga, querySaga) {
@@ -29,31 +29,34 @@ export function actionToPath(routesMap, action) {
 		return null;
 	}
 
-	const path = route.toPath(action.params) || '/';
+	const pathname = route.toPath(action.params) || '/';
 
 	const query = new URLSearchParams(action.query ?? {});
 	query.sort();
+	const search = Array.from(query.keys()).length ? `?${query.toString()}` : '';
 
-	return `${path}${Array.from(query.keys()).length ? `?${query.toString()}` : ''}`;
+	return { pathname, search };
 }
 
 export const NOT_FOUND = 'NOT_FOUND';
 
-export function pathToAction(routesMap, path, search, state) {
-	if (typeof path !== 'string') {
+export function pathToAction(routesMap, location) {
+	const { pathname, search } = location ?? {};
+
+	if (typeof pathname !== 'string') {
 		return null;
 	}
 
 	for (const [id, route] of routesMap.entries()) {
-		const match = route.match(path);
+		const match = route.match(pathname);
 		if (match) {
 			const query = new URLSearchParams(search ?? '');
 			query.sort();
 
 			return navigate(
 				id,
-				{ ...match.params, ...(state ?? {}) },
-				{ ...(Array.from(query.keys()).length ? { query: Object.fromEntries(query) } : {}) }
+				{ ...match.params },
+				{ query: Array.from(query.keys()).length ? Object.fromEntries(query) : null }
 			);
 		}
 	}
