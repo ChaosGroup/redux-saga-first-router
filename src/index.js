96208ad371fffab5bbe5f47eb5f72cb7c14a584f
Ivo Stefchev <ivakaetuk@gmail.com>
2020-07-30 23:51:43 +0300

    Fix init of regenerator-runtime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime.js";
 import { NAVIGATE, navigate, route, buildRoutesMap, actionToPath, pathToAction } from './core';
 import { reducer } from './reducer';
 import { saga } from './saga';
